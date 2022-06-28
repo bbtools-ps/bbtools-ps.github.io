3fbc44484de87a49c877a6038306365d4632ec4c
Bogdan Bogdanovic <bogdi.mail@gmail.com>
2022-06-28 22:13:31 +0200

    returned old static website

---
+++ js/main.js
@@ -0,0 +1,26 @@
+// Function for fixing runts:
+// https://github.com/bbtools-ps/javascript-test-projects/blob/main/fix_runts.js
+
+
+// Function that fixes runts from the string (paragraph). It replaces the last whitespace character with the &nbsp; (non-breaking space character)
+function fixRunts(str) {
+    // If string has any html tags inside then just return it
+    if (!str.match(/\<[a-z]+/)) {
+        return str.replace(/\s+(\S+)$/gim, `&nbsp;$1`);
+    } else {
+        return str;
+    }
+}
+(function () {
+    // Fix runts from all paragraphs
+    let paragraphs = document.getElementsByTagName("p");
+    for (let i = 0; i < paragraphs.length; i++) {
+        paragraphs[i].innerHTML = fixRunts(paragraphs[i].innerHTML);
+    }
+    // Fix runts from all list items
+    let listItems = document.getElementsByTagName("li");
+    for (let i = 0; i < listItems.length; i++) {
+        listItems[i].innerHTML = fixRunts(listItems[i].innerHTML);
+    }
+
+})();
